060c94b1fea8f05177805a129895ee8046addfdc
Hitomamacs <macsdeve@gmail.com>
2022-10-16 21:59:06 +0200

    Update draft cup detection & topdown view.py

--- draft cup detection & topdown view.py
+++ draft cup detection & topdown view.py
@@ -18,6 +18,7 @@ capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 1280)
 checkk = True
 while True:
+    rendered = np.zeros((1280, 1280, 3), dtype = "uint8")
     check, frame = capture.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 100)
@@ -26,16 +27,20 @@ while True:
         for (x, y, r) in circles:
             cv2.circle(frame, (x, y), r, (0, 255, 0), 4)
             cv2.rectangle(frame, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+            cv2.circle(rendered, (x,y), r, color = (0,0,255), thickness= -1)
+    
+    
     if (len(posList) == 4) and (checkk == True) :
         for pos in posList:
             frame = cv2.circle(frame, pos, radius=10, color=(0, 0, 255), thickness=-1)
         pts1 = np.float32([[posList[0][0],posList[0][1]], [posList[1][0], posList[1][1]], [posList[2][0], posList[2][1]], [posList[3][0], posList[3][1]]])
         pts2 = np.float32([[0,0],[1280,0], [0, 1280], [1280,1280]])
         matrix = cv2.getPerspectiveTransform(pts1,pts2)
-        print(pts1)
         image = cv2.warpPerspective(frame, matrix, (1280,1280))
+        rendered = cv2.warpPerspective(rendered, matrix, (1280,1280))          
         output = image.copy()
         cv2.imshow("persp", image) 
+        cv2.imshow("rendered", rendered)          
      
        
     cv2.imshow('mouseRGB', frame)
